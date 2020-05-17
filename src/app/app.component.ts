ee40f0f2b90ac71a5287a601fb4339aad2904415
Marek Jaczewski <jaczewski147@gmail.com>
2020-05-17 18:57:26 +0200

    add features for remove and checking tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,20 @@ export class AppComponent {
 
   newTask: string;
   taskList: Array<string> = [];
-    
-  add(){
+  taskDone: Array<string> = [];
+
+  add() {
     this.taskList.push(this.newTask);
     this.newTask = "";
-    console.log(this.taskList)
+  }
+
+  remove(task: string) {
+    this.taskList = this.taskList.filter(e => e !== task)
+  }
+
+  done(task: string) {
+    this.taskDone.push(task);
+    this.remove(task);
   }
 
 }
