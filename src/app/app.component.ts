917cfca0f247818069bf89801853e9d2d72f27fc
Marek Jaczewski <jaczewski147@gmail.com>
2020-05-17 18:18:22 +0200

    add feature for creating tasks

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  newTask: string;
+  taskList: Array<string> = [];
+    
+  add(){
+    this.taskList.push(this.newTask);
+    this.newTask = "";
+    console.log(this.taskList)
+  }
+
+}
